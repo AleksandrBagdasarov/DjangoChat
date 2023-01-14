3d6d5ce45ea2eec40ba36dac60bec66993796c9f
Bahdasarov <sashabagdasarov@gmail.com>
2023-01-14 02:22:19 +0200

    move to py3.11

--- chat-backend/scheduler.py
+++ chat-backend/scheduler.py
@@ -1,22 +1,29 @@
 import asyncio
 import json
 import logging
+from datetime import datetime, timezone, tzinfo
+from threading import Thread
+from time import sleep
 from typing import Union
 
 import psycopg2
 import psycopg2.extensions
+import psycopg2.extras
+import requests
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from chat.settings import DATABASES
 from websockets import connect
 
-print(DATABASES)
-
 DSN = {
-    "database": DATABASES["default"]["NAME"],
+    "dbname": DATABASES["default"]["NAME"],
     "user": DATABASES["default"]["USER"],
     "password": DATABASES["default"]["PASSWORD"],
 }
-CONN = psycopg2.connect(DSN)
+dbname = DSN["dbname"]
+user = DSN["user"]
+password = DSN["password"]
+
+print(DSN)
 
 
 class LoggingCursor(psycopg2.extensions.cursor):
@@ -39,24 +46,70 @@ async def send_to_chat(payload: dict, chat_id: Union[int, str]):
         await ws.send(payload)
 
 
+async def test_(job):
+    print(f"start       : {job.id}")
+    now = datetime.now(tz=timezone.utc)
+    now_time_start = now.strftime("%H:%M:%S")
+
+    job.execute_at.replace(tzinfo=timezone.utc)
+    execute_at_time = job.execute_at.strftime("%H:%M:%S")
+    execute_in = (job.execute_at - now).total_seconds()
+    if execute_in > 0:
+        sleep(int(execute_in))
+    now = datetime.now(tz=timezone.utc)
+    now_time_end = now.strftime("%H:%M:%S")
+    print(f"start       : {now_time_start}")
+    print(f"end         : {now_time_end}")
+    print(f"execute_at  : {execute_at_time}")
+    print(f"execute_in  : {execute_in}")
+    print()
+    print("#" * 20)
+
+
+def adapter(job):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(test_(job))
+    loop.close()
+
+
 async def get_jobs():
-    # sql = """
-    # select * from
-    # """
-    cur = CONN.cursor(DSN, cursor_factory=LoggingCursor)
-    cur.execute()
-    jobs = cur.fetch_all()
-    print(jobs)
+    sql = """
+    select * from scheduled_message
+    where execute_at < now() + interval '1 minute'
+    and executed = false
+    """
+    CONN = psycopg2.connect(
+        f"dbname={dbname} user={user} password={password} host=localhost"
+    )
+    cur = CONN.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+    cur.execute(
+        sql,
+    )
+    jobs = cur.fetchall()
+    tasks = set()
+    for job in jobs:
+        t = Thread(target=adapter, args=[job])
+        t.start()
+        # task = asyncio.create_task(test_(job))
+        # tasks.add(task)
+        # now = datetime.now(tz=timezone.utc)
+    # await asyncio.gather(*tasks)
+    # print("execute_at:", job.execute_at.strftime('%H:%M:%S'))
+    # print("now       :", now.strftime('%H:%M:%S'))
+    # print("execute in:", (job.execute_at - now).total_seconds())
+    # print("#" * 10)
+    print("jobs", jobs)
+    print(". " * 10)
 
-
-# asyncio.run(send_to_chat())
 
 if __name__ == "__main__":
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(get_jobs, "interval", seconds=60)
-    scheduler.start()
-    try:
-        asyncio.get_event_loop().run_forever()
-    except (KeyboardInterrupt, SystemExit):
-        print("Keyboard interrupt...")
-        pass
+    # scheduler = AsyncIOScheduler()
+    # scheduler.add_job(get_jobs, "interval", seconds=60)
+    # scheduler.start()
+    # try:
+    #     asyncio.get_event_loop().run_forever()
+    # except (KeyboardInterrupt, SystemExit):
+    #     print("Keyboard interrupt...")
+    #     pass
+    asyncio.run(get_jobs())
